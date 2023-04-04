112d22a1b124e7266b6216bd4a1952669641f559
Michael Gerhold <michael.gerhold@web.de>
2023-04-04 16:12:50 +0200

    moved definitions from `tetromino.cpp` into header

--- src/tetromino.hpp
+++ src/tetromino.hpp
@@ -45,7 +45,11 @@ public:
           m_type{ type },
           m_minos{ create_minos(position, m_rotation, type) } { }
 
-    void render(const Application& app, const Grid& grid) const;
+    void render(const Application& app, const Grid& grid) const {
+        for (const auto& mino : m_minos) {
+            mino.render(app, grid);
+        }
+    }
 
     void rotate_right() {
         ++m_rotation;
@@ -101,5 +105,57 @@ private:
 
     using TetrominoPatterns = std::array<Pattern, 4>; // one pattern per rotation
 
-    static std::array<TetrominoPatterns, 7> tetrominos;
+    // clang-format off
+    static constexpr auto tetrominos = std::array<Tetromino::TetrominoPatterns, 7>{
+        // I
+        TetrominoPatterns{
+                          Pattern{ Point{ 0, 1 }, Point{ 1, 1 }, Point{ 2, 1 }, Point{ 3, 1 }, },
+                          Pattern{ Point{ 2, 0 }, Point{ 2, 1 }, Point{ 2, 2 }, Point{ 2, 3 }, },
+                          Pattern{ Point{ 0, 2 }, Point{ 1, 2 }, Point{ 2, 2 }, Point{ 3, 2 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 1, 1 }, Point{ 1, 2 }, Point{ 1, 3 }, },
+                          },
+        // J
+        TetrominoPatterns{
+                          Pattern{ Point{ 0, 0 }, Point{ 0, 1 }, Point{ 1, 1 }, Point{ 2, 1 }, },
+                          Pattern{ Point{ 2, 0 }, Point{ 1, 0 }, Point{ 1, 1 }, Point{ 1, 2 }, },
+                          Pattern{ Point{ 0, 1 }, Point{ 1, 1 }, Point{ 2, 1 }, Point{ 2, 2 }, },
+                          Pattern{ Point{ 0, 2 }, Point{ 1, 2 }, Point{ 1, 1 }, Point{ 1, 0 }, },
+                          },
+        // L
+        TetrominoPatterns{
+                          Pattern{ Point{ 0, 1 }, Point{ 1, 1 }, Point{ 2, 1 }, Point{ 2, 0 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 1, 1 }, Point{ 1, 2 }, Point{ 2, 2 }, },
+                          Pattern{ Point{ 0, 2 }, Point{ 0, 1 }, Point{ 1, 1 }, Point{ 2, 1 }, },
+                          Pattern{ Point{ 0, 0 }, Point{ 1, 0 }, Point{ 1, 1 }, Point{ 1, 2 }, },
+                          },
+        // O
+        TetrominoPatterns{
+                          Pattern{ Point{ 1, 0 }, Point{ 2, 0 }, Point{ 1, 1 }, Point{ 2, 1 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 2, 0 }, Point{ 1, 1 }, Point{ 2, 1 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 2, 0 }, Point{ 1, 1 }, Point{ 2, 1 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 2, 0 }, Point{ 1, 1 }, Point{ 2, 1 }, },
+                          },
+        // S
+        TetrominoPatterns{
+                          Pattern{ Point{ 0, 1 }, Point{ 1, 1 }, Point{ 1, 0 }, Point{ 2, 0 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 1, 1 }, Point{ 2, 1 }, Point{ 2, 2 }, },
+                          Pattern{ Point{ 0, 2 }, Point{ 1, 2 }, Point{ 1, 1 }, Point{ 2, 1 }, },
+                          Pattern{ Point{ 0, 0 }, Point{ 0, 1 }, Point{ 1, 1 }, Point{ 1, 2 }, },
+                          },
+        // T
+        TetrominoPatterns{
+                          Pattern{ Point{ 0, 1 }, Point{ 1, 1 }, Point{ 1, 0 }, Point{ 2, 1 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 1, 1 }, Point{ 2, 1 }, Point{ 1, 2 }, },
+                          Pattern{ Point{ 0, 1 }, Point{ 1, 1 }, Point{ 2, 1 }, Point{ 1, 2 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 1, 1 }, Point{ 0, 1 }, Point{ 1, 2 }, },
+                          },
+        // Z
+        TetrominoPatterns{
+                          Pattern{ Point{ 0, 0 }, Point{ 1, 0 }, Point{ 1, 1 }, Point{ 2, 1 }, },
+                          Pattern{ Point{ 2, 0 }, Point{ 2, 1 }, Point{ 1, 1 }, Point{ 1, 2 }, },
+                          Pattern{ Point{ 0, 1 }, Point{ 1, 1 }, Point{ 1, 2 }, Point{ 2, 2 }, },
+                          Pattern{ Point{ 1, 0 }, Point{ 1, 1 }, Point{ 0, 1 }, Point{ 0, 2 }, },
+                          },
+    };
+    // clang-format on
 };
