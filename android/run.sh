05a3cece23480fc2524684eb6b439ffe6ec795e5
Totto16 <tobiausgais@gmail.com>
2023-04-11 00:50:50 +0200

    it now builds and compiles, but the application itself needs some special cases handling

--- android/run.sh
+++ android/run.sh
@@ -19,83 +19,82 @@ if [ ! -d "$BASE_PATH" ]; then
     cd ..
 fi
 
-export ARCH="arm"
-export ARM_VERSION="armv7a"
-export ABI="armeabi-v7a"
+ARCH_LIST=("arm" "aarch64")
+ARM_VERSION_LIST=("armv7a" "aarch64")
+ARM_NAME_LIST=("androideabi" "android")
+ARM_TARET_ARCH_LIST=("armeabi-v7a" "arm64-v8a")
 
-export SDK_VERSION="33"
+for INDEX in "${!ARCH_LIST[@]}"; do
 
-export ANDROID_NDK_HOME="$BASE_PATH"
-export ANDROID_NDK="$BASE_PATH"
+    export ARCH=${ARCH_LIST[$INDEX]}
+    export ARM_VERSION=${ARM_VERSION_LIST[$INDEX]}
+    export ARM_NAME=${ARM_NAME_LIST[$INDEX]}
+    export ARM_TARET_ARCH=${ARM_TARET_ARCH_LIST[$INDEX]}
+    export ARM_TRIPLE=$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION
 
-export HOST_ROOT="$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64"
-# export SYS_ROOT="${HOST_ROOT}/sysroot"
-export SYS_ROOT="${HOST_ROOT}/sysroot_sym"
-export BIN_DIR="$HOST_ROOT/bin"
-export PATH="$BIN_DIR:$PATH"
+    export SDK_VERSION="33"
 
-LIB_PATH="${SYS_ROOT}/usr/lib/${ARCH}-linux-androideabi:${SYS_ROOT}/usr/lib/${ARCH}-linux-androideabi/${SDK_VERSION}"
-INC_PATH="${SYS_ROOT}/usr/include"
+    export ANDROID_NDK_HOME="$BASE_PATH"
+    export ANDROID_NDK="$BASE_PATH"
 
-# ln -s "${SYS_ROOT}/usr/lib/${ARCH}-linux-androideabi/${SDK_VERSION}" "$SYS_ROOT/usr"
-# mv "$SYS_ROOT/usr/${SDK_VERSION}" "$SYS_ROOT/usr/lib_sym"
+    export SYM_LINK_PATH=sysroot_sym-$ARM_VERSION
 
-# XXX I not sure how much of this cmake config is actually having an impact
-export CMAKE_PREFIX_PATH="$SYS_ROOT"
-export CMAKE_LIBRARY_PATH="$LIB_PATH"
-export CMAKE_INCLUDE_PATH="$INC_PATH"
+    export HOST_ROOT="$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64"
+    # export SYS_ROOT="${HOST_ROOT}/sysroot"
+    export SYS_ROOT="${HOST_ROOT}/$SYM_LINK_PATH"
+    export BIN_DIR="$HOST_ROOT/bin"
+    export PATH="$BIN_DIR:$PATH"
 
-export LIBRARY_PATH="$SYS_ROOT/usr/lib/$ARCH-linux-androideabi/$SDK_VERSION"
+    LIB_PATH="${SYS_ROOT}/usr/lib/${ARCH}-linux-$ARM_NAME:${SYS_ROOT}/usr/lib/${ARCH}-linux-$ARM_NAME/${SDK_VERSION}"
+    INC_PATH="${SYS_ROOT}/usr/include"
 
-# export CFLAGS="-DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN} \
-#   -DANDROID_STL=c++_shared \
-#   -DANDROID_TOOLCHAIN=clang \
-#   -DANDROID_PLATFORM=android-${SDK_VERSION} \
-#   -DANDROID_ABI=${ABI} \
-#   -isystems '$LIBRARY_PATH' \
-#   -isystem'usr/lib/$ARCH-linux-androideabi/$SDK_VERSION'
-#   "
-# export CXXFLAGS="$CFLAGS"
+    # XXX I not sure how much of this cmake config is actually having an impact
+    export CMAKE_PREFIX_PATH="$SYS_ROOT"
+    export CMAKE_LIBRARY_PATH="$LIB_PATH"
+    export CMAKE_INCLUDE_PATH="$INC_PATH"
 
-LAST_DIR=$PWD
+    export LIBRARY_PATH="$SYS_ROOT/usr/lib/$ARCH-linux-$ARM_NAME/$SDK_VERSION"
 
-if [ ! -d "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/" ]; then
+    LAST_DIR=$PWD
 
-    mkdir -p "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/lib"
+    if [ -d "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/" ]; then
 
-    cd "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/"
+        rm -rf "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/"
+    fi
 
-    ln -s "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/local" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/"
+    mkdir -p "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib"
 
-    ln -s "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/"
+    cd "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/"
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-androideabi/" -maxdepth 1 -name "*.so" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/lib/" \;
+    ln -s "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/local" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/"
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-androideabi/" -maxdepth 1 -name "*.a" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/lib/" \;
+    ln -s "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/"
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-androideabi/$SDK_VERSION/" -maxdepth 1 -name "*.a" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/lib/" \;
+    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/" -maxdepth 1 -name "*.so" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-androideabi/$SDK_VERSION/" -maxdepth 1 -name "*.so" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/lib/" \;
+    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/" -maxdepth 1 -name "*.a" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-androideabi/$SDK_VERSION/" -maxdepth 1 -name "*.o" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym/usr/lib/" \;
+    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/$SDK_VERSION/" -maxdepth 1 -name "*.a" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
+
+    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/$SDK_VERSION/" -maxdepth 1 -name "*.so" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
+
+    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/$SDK_VERSION/" -maxdepth 1 -name "*.o" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
 
     cd "$LAST_DIR"
 
-fi
+    export BUILD_DIR="build-$ARM_TARET_ARCH"
 
-export BUILD_DIR="build"
+    export SDL2_VERSION="2.26.0"
 
-export SDL2_VERSION="2.26.0"
+    export CC=$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION-clang
+    export CPP=$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION-clang++
+    export CXX=$CPP
+    export LD=llvm-ld
+    export AS=llvm-as
+    export AR=llvm-ar
+    export RANLIB=llvm-ranlib
 
-export CC=armv7a-linux-androideabi$SDK_VERSION-clang
-export CPP=armv7a-linux-androideabi$SDK_VERSION-clang++
-export CXX=$CPP
-export LD=llvm-ld
-export AS=llvm-as
-export AR=llvm-ar
-export RANLIB=llvm-ranlib
-
-cat <<EOF >./android/crossbuilt.ini
+    cat <<EOF >"./android/crossbuilt-$ARM_TARET_ARCH.ini"
 [host_machine]
 system = 'android'
 cpu_family = '$ARCH'
@@ -104,11 +103,11 @@ endian = 'little'
 
 [constants]
 android_ndk = '$BIN_DIR'
-toolchain = '$BIN_DIR/armv7a-linux-androideabi-$SDK_VERSION'
+toolchain = '$BIN_DIR/$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION'
 
 [binaries]
-c = 'armv7a-linux-androideabi$SDK_VERSION-clang'
-cpp = 'armv7a-linux-androideabi$SDK_VERSION-clang++'
+c = '$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION-clang'
+cpp = '$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION-clang++'
 ar      = 'llvm-ar'
 as      = 'llvm-as'
 ranlib  = 'llvm-ranlib'
@@ -121,8 +120,8 @@ llvm-config = 'llvm-config'
 [built-in options]
 c_std = 'c11'
 cpp_std = 'c++20'
-c_args = ['--sysroot=$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym','-fPIE','-fPIC','--target=$ARM_VERSION-none-linux-androideabi','-DHAVE_USR_INCLUDE_MALLOC_H','-D_MALLOC_H', '-I$BASE_PATH/sources/android/cpufeatures/']
-cpp_args = ['--sysroot=$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym','-fPIE','-fPIC','--target=$ARM_VERSION-none-linux-androideabi']
+c_args = ['--sysroot=$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH','-fPIE','-fPIC','--target=$ARM_VERSION-none-linux-$ARM_NAME','-DHAVE_USR_INCLUDE_MALLOC_H','-D_MALLOC_H', '-I$BASE_PATH/sources/android/cpufeatures/']
+cpp_args = ['--sysroot=$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH','-fPIE','-fPIC','--target=$ARM_VERSION-none-linux-$ARM_NAME']
 c_link_args = ['-fPIE']
 cpp_link_args = ['-fPIE']
 prefix = '$SYS_ROOT'
@@ -130,44 +129,49 @@ libdir = '$LIB_PATH'
 
 [properties]
 pkg_config_libdir = '$LIB_PATH'
-sys_root = '$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot_sym'
+sys_root = '$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH'
 
 EOF
 
-export LIBRARY_PATH="$LIBRARY_PATH:usr/lib/$ARCH-linux-androideabi/$SDK_VERSION:$LIB_PATH"
+    export LIBRARY_PATH="$LIBRARY_PATH:usr/lib/$ARCH-linux-$ARM_NAME/$SDK_VERSION:$LIB_PATH"
 
-set +e
+    export LD_FLAGS="-Wl,--no-undefined"
 
-meson setup $BUILD_DIR \
-    "--prefix=$SYS_ROOT" \
-    "--includedir=$INC_PATH" \
-    "--libdir=usr/lib/$ARCH-linux-androideabi/$SDK_VERSION" \
-    "--build.cmake-prefix-path=$SYS_ROOT" \
-    --cross-file ./android/crossbuilt.ini \
-    -Dsdl2:use_hidapi=disabled \
-    -Dsdl2:test=false
+    set +e
 
-EXIT_CODE=$?
-
-if [ $EXIT_CODE -ne 0 ]; then
-
-    set -e
-
-    if [ ! -e "$PWD/subprojects/SDL2-$SDL2_VERSION/src/core/android/cpu-features.c" ]; then
-        ln -s "$BASE_PATH/sources/android/cpufeatures/cpu-features.c" "$PWD/subprojects/SDL2-$SDL2_VERSION/src/core/android/cpu-features.c"
-    fi
-
-    rm -rf $BUILD_DIR
-
-    meson setup $BUILD_DIR \
+    meson setup "$BUILD_DIR" \
         "--prefix=$SYS_ROOT" \
         "--includedir=$INC_PATH" \
-        "--libdir=usr/lib/$ARCH-linux-androideabi/$SDK_VERSION" \
+        "--libdir=usr/lib/$ARCH-linux-$ARM_NAME/$SDK_VERSION" \
         "--build.cmake-prefix-path=$SYS_ROOT" \
-        --cross-file ./android/crossbuilt.ini
+        --cross-file "./android/crossbuilt-$ARM_TARET_ARCH.ini" \
+        -Db_asneeded=false -Db_lundef=false \
+        -Dsdl2:use_hidapi=disabled \
+        -Dsdl2:test=false
 
-fi
+    EXIT_CODE=$?
+    set -e
 
-meson compile -C $BUILD_DIR
+    if [ $EXIT_CODE -ne 0 ]; then
 
-llvm-readelf --needed-libs ./build/liboopetris.so
+        if [ ! -e "$PWD/subprojects/SDL2-$SDL2_VERSION/src/core/android/cpu-features.c" ]; then
+            ln -s "$BASE_PATH/sources/android/cpufeatures/cpu-features.c" "$PWD/subprojects/SDL2-$SDL2_VERSION/src/core/android/cpu-features.c"
+        fi
+
+        rm -rf "$BUILD_DIR"
+
+        meson setup "$BUILD_DIR" \
+            "--prefix=$SYS_ROOT" \
+            "--includedir=$INC_PATH" \
+            "--libdir=usr/lib/$ARCH-linux-$ARM_NAME/$SDK_VERSION" \
+            "--build.cmake-prefix-path=$SYS_ROOT" \
+            --cross-file "./android/crossbuilt-$ARM_TARET_ARCH.ini" \
+            -Db_asneeded=false -Db_lundef=false \
+            -Dsdl2:use_hidapi=disabled \
+            -Dsdl2:test=false
+
+    fi
+
+    meson compile -C "$BUILD_DIR"
+
+done
