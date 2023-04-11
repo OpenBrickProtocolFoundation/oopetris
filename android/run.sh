3daca054d6d1e784063244a0a2149a6b72f6baf9
Totto16 <tobiausgais@gmail.com>
2023-04-12 00:03:43 +0200

    - use defintions of build targets from abis.json (from ndk) - geenrate all 4 targets

--- android/run.sh
+++ android/run.sh
@@ -5,8 +5,11 @@ set -e
 mkdir -p toolchains
 
 export NDK_VER="25c"
+export SDK_VERSION="33"
 
 export BASE_PATH="$PWD/toolchains/android-ndk-r$NDK_VER"
+export ANDROID_NDK_HOME="$BASE_PATH"
+export ANDROID_NDK="$BASE_PATH"
 
 if [ ! -d "$BASE_PATH" ]; then
 
@@ -19,68 +22,74 @@ if [ ! -d "$BASE_PATH" ]; then
     cd ..
 fi
 
-ARCH_LIST=("arm" "aarch64")
-ARM_VERSION_LIST=("armv7a" "aarch64")
-ARM_NAME_LIST=("androideabi" "android")
-ARM_TARET_ARCH_LIST=("armeabi-v7a" "arm64-v8a")
+if [ ! -e "$BASE_PATH/meta/abis.json" ]; then
 
-for INDEX in "${!ARCH_LIST[@]}"; do
+    echo "no abis.json file found, to determine supported abis"
+    exit 2
 
-    export ARCH=${ARCH_LIST[$INDEX]}
-    export ARM_VERSION=${ARM_VERSION_LIST[$INDEX]}
-    export ARM_NAME=${ARM_NAME_LIST[$INDEX]}
-    export ARM_TARET_ARCH=${ARM_TARET_ARCH_LIST[$INDEX]}
-    export ARM_TRIPLE=$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION
+fi
 
-    export SDK_VERSION="33"
+echo
 
-    export ANDROID_NDK_HOME="$BASE_PATH"
-    export ANDROID_NDK="$BASE_PATH"
+mapfile -t ARCH_KEYS < <(jq 'keys' -M -r -c "$BASE_PATH/meta/abis.json" | tr -d '[]"' | sed 's/,/\n/g')
+
+for INDEX in "${!ARCH_KEYS[@]}"; do
+    export KEY=${ARCH_KEYS[$INDEX]}
+
+    RAW_JSON=$(jq '.[$KEY]' -M -r -c --arg KEY "$KEY" "$BASE_PATH/meta/abis.json")
+
+    BITNESS=$(echo "$RAW_JSON" | jq -M -r -c '."bitness"') || true
+    ARCH=$(echo "$RAW_JSON" | jq -M -r -c '."arch"')
+    ARM_VERSION=$(echo "$RAW_JSON" | jq -M -r -c '."proc"' | tr -d "-")
+    ARM_NAME_TRIPLE=$(echo "$RAW_JSON" | jq -M -r -c '."triple"')
+    ARM_TARET_ARCH=$KEY
+    ARM_TRIPLE=$ARM_NAME_TRIPLE$SDK_VERSION
+    ARM_COMPILER_TRIPLE=$(echo "$RAW_JSON" | jq -M -r -c '."llvm_triple"')
+    ARM_TOOL_TRIPLE=$(echo "$ARM_NAME_TRIPLE$SDK_VERSION" | sed s/$ARCH/$ARM_VERSION/)
 
     export SYM_LINK_PATH=sysroot_sym-$ARM_VERSION
 
     export HOST_ROOT="$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64"
-    # export SYS_ROOT="${HOST_ROOT}/sysroot"
     export SYS_ROOT="${HOST_ROOT}/$SYM_LINK_PATH"
     export BIN_DIR="$HOST_ROOT/bin"
     export PATH="$BIN_DIR:$PATH"
 
-    LIB_PATH="${SYS_ROOT}/usr/lib/${ARCH}-linux-$ARM_NAME:${SYS_ROOT}/usr/lib/${ARCH}-linux-$ARM_NAME/${SDK_VERSION}"
+    LIB_PATH="${SYS_ROOT}/usr/lib/$ARM_TRIPLE:${SYS_ROOT}/usr/lib/$ARM_TRIPLE/${SDK_VERSION}"
     INC_PATH="${SYS_ROOT}/usr/include"
 
-    export LIBRARY_PATH="$SYS_ROOT/usr/lib/$ARCH-linux-$ARM_NAME/$SDK_VERSION"
+    export LIBRARY_PATH="$SYS_ROOT/usr/lib/$ARM_NAME_TRIPLE/$SDK_VERSION"
 
     LAST_DIR=$PWD
 
-    if [ -d "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/" ]; then
+    if [ -d "${SYS_ROOT:?}/" ]; then
 
-        rm -rf "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/"
+        rm -rf "${SYS_ROOT:?}/"
     fi
 
-    mkdir -p "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib"
+    mkdir -p "${SYS_ROOT:?}/usr/lib"
 
-    cd "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/"
+    cd "${SYS_ROOT:?}/usr/"
 
-    ln -s "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/local" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/"
+    ln -s "$HOST_ROOT/sysroot/usr/local" "${SYS_ROOT:?}/usr/"
 
-    ln -s "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/"
+    ln -s "$HOST_ROOT/sysroot/usr/include" "${SYS_ROOT:?}/usr/"
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/" -maxdepth 1 -name "*.so" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
+    find "$HOST_ROOT/sysroot/usr/lib/$ARM_NAME_TRIPLE/" -maxdepth 1 -name "*.so" -exec ln -s "{}" "${SYS_ROOT:?}/usr/lib/" \;
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/" -maxdepth 1 -name "*.a" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
+    find "$HOST_ROOT/sysroot/usr/lib/$ARM_NAME_TRIPLE/" -maxdepth 1 -name "*.a" -exec ln -s "{}" "${SYS_ROOT:?}/usr/lib/" \;
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/$SDK_VERSION/" -maxdepth 1 -name "*.a" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
+    find "$HOST_ROOT/sysroot/usr/lib/$ARM_NAME_TRIPLE/$SDK_VERSION/" -maxdepth 1 -name "*.a" -exec ln -s "{}" "${SYS_ROOT:?}/usr/lib/" \;
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/$SDK_VERSION/" -maxdepth 1 -name "*.so" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
+    find "$HOST_ROOT/sysroot/usr/lib/$ARM_NAME_TRIPLE/$SDK_VERSION/" -maxdepth 1 -name "*.so" -exec ln -s "{}" "${SYS_ROOT:?}/usr/lib/" \;
 
-    find "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${ARCH}-linux-$ARM_NAME/$SDK_VERSION/" -maxdepth 1 -name "*.o" -exec ln -s "{}" "$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH/usr/lib/" \;
+    find "$HOST_ROOT/sysroot/usr/lib/$ARM_NAME_TRIPLE/$SDK_VERSION/" -maxdepth 1 -name "*.o" -exec ln -s "{}" "${SYS_ROOT:?}/usr/lib/" \;
 
     cd "$LAST_DIR"
 
     export BUILD_DIR="build-$ARM_TARET_ARCH"
 
-    export CC=$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION-clang
-    export CPP=$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION-clang++
+    export CC=$ARM_TRIPLE-clang
+    export CPP=$ARM_TRIPLE-clang++
     export CXX=$CPP
     export LD=llvm-ld
     export AS=llvm-as
@@ -96,11 +105,11 @@ endian = 'little'
 
 [constants]
 android_ndk = '$BIN_DIR'
-toolchain = '$BIN_DIR/$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION'
+toolchain = '$BIN_DIR/$ARM_TRIPLE'
 
 [binaries]
-c = '$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION-clang'
-cpp = '$ARM_VERSION-linux-$ARM_NAME$SDK_VERSION-clang++'
+c = '$ARM_TOOL_TRIPLE-clang'
+cpp = '$ARM_TOOL_TRIPLE-clang++'
 ar      = 'llvm-ar'
 as      = 'llvm-as'
 ranlib  = 'llvm-ranlib'
@@ -113,8 +122,8 @@ llvm-config = 'llvm-config'
 [built-in options]
 c_std = 'c11'
 cpp_std = 'c++20'
-c_args = ['--sysroot=$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH','-fPIE','-fPIC','--target=$ARM_VERSION-none-linux-$ARM_NAME','-DHAVE_USR_INCLUDE_MALLOC_H','-D_MALLOC_H', '-I$BASE_PATH/sources/android/cpufeatures/']
-cpp_args = ['--sysroot=$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH','-fPIE','-fPIC','--target=$ARM_VERSION-none-linux-$ARM_NAME']
+c_args = ['--sysroot=${SYS_ROOT:?}','-fPIE','-fPIC','--target=$ARM_COMPILER_TRIPLE','-DHAVE_USR_INCLUDE_MALLOC_H','-D_MALLOC_H','-D__BITNESS=$BITNESS']
+cpp_args = ['--sysroot=${SYS_ROOT:?}','-fPIE','-fPIC','--target=$ARM_COMPILER_TRIPLE','-D__BITNESS=$BITNESS']
 c_link_args = ['-fPIE']
 cpp_link_args = ['-fPIE']
 prefix = '$SYS_ROOT'
@@ -122,7 +131,7 @@ libdir = '$LIB_PATH'
 
 [properties]
 pkg_config_libdir = '$LIB_PATH'
-sys_root = '$BASE_PATH/toolchains/llvm/prebuilt/linux-x86_64/$SYM_LINK_PATH'
+sys_root = '${SYS_ROOT}'
 
 EOF
 
@@ -149,12 +158,12 @@ meson.override_dependency(
 EOF
     fi
 
-    export LIBRARY_PATH="$LIBRARY_PATH:usr/lib/$ARCH-linux-$ARM_NAME/$SDK_VERSION:$LIB_PATH"
+    export LIBRARY_PATH="$LIBRARY_PATH:usr/lib/$ARM_NAME_TRIPLE/$SDK_VERSION:$LIB_PATH"
 
     meson setup "$BUILD_DIR" \
         "--prefix=$SYS_ROOT" \
         "--includedir=$INC_PATH" \
-        "--libdir=usr/lib/$ARCH-linux-$ARM_NAME/$SDK_VERSION" \
+        "--libdir=usr/lib/$ARM_NAME_TRIPLE/$SDK_VERSION" \
         --cross-file "./android/crossbuilt-$ARM_TARET_ARCH.ini" \
         -Dsdl2:use_hidapi=disabled \
         -Dsdl2:test=false
