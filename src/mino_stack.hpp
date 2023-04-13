4e67c1e4c5ca657f94f6c0257365f122460509c1
Michael Gerhold <michael.gerhold@web.de>
2023-04-13 18:16:45 +0200

    replaced calls to functions in `std::ranges` with "regular" algorithms

--- src/mino_stack.hpp
+++ src/mino_stack.hpp
@@ -6,12 +6,6 @@
 #include <iostream>
 #include <magic_enum.hpp>
 #include <vector>
-#if defined(__ANDROID__)
-#include "ranges.hpp"
-#include <algorithm>
-#else
-#include <ranges>
-#endif
 
 struct Grid;
 
@@ -34,39 +28,22 @@ public:
     }
 
     [[nodiscard]] bool operator==(const MinoStack& other) const {
-        using std::ranges::all_of, std::ranges::find, std::ranges::end;
-
         if (m_minos.size() != other.m_minos.size()) {
             return false;
         }
 
-
-#if defined(__ANDROID__)
         const auto all_of_this_in_other = std::all_of(m_minos.cbegin(), m_minos.cend(), [&](const auto& mino) {
             return std::find(other.m_minos.cbegin(), other.m_minos.cend(), mino) != end(other.m_minos);
         });
-#else
-        const auto all_of_this_in_other =
-                all_of(m_minos, [&](const auto& mino) { return find(other.m_minos, mino) != end(other.m_minos); });
-#endif
 
         if (not all_of_this_in_other) {
             return false;
         }
 
-
-#if defined(__ANDROID__)
         const auto all_of_other_in_this =
                 std::all_of(other.m_minos.cbegin(), other.m_minos.cend(), [&](const auto& mino) {
                     return std::find(m_minos.cbegin(), m_minos.cend(), mino) != end(m_minos);
                 });
-
-
-#else
-        const auto all_of_other_in_this =
-                all_of(other.m_minos, [&](const auto& mino) { return find(m_minos, mino) != end(m_minos); });
-
-#endif
 
         return all_of_other_in_this;
     }
