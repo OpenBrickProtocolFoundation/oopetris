1508416fb6a5d7a021412b833ac3c1e329e256b4
Totto16 <tobiausgais@gmail.com>
2023-04-16 01:48:33 +0200

    fix conversion from std::filesystem::path to char *

--- src/font.cpp
+++ src/font.cpp
@@ -9,7 +9,7 @@ Font::Font(const std::string& path, int size) {
 }
 
 Font::Font(const std::filesystem::path& path, int size) {
-    m_font = TTF_OpenFont(path.c_str(), size);
+    m_font = TTF_OpenFont(path.string().c_str(), size);
     if (m_font == nullptr) {
         throw FontLoadingError{ "error loading font: '" + std::string{ TTF_GetError() } + "'" };
     }
