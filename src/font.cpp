772f887f82a65a3d3ca3e6c4e09dd492e39dd79d
Michael Gerhold <michael.gerhold@web.de>
2023-04-16 15:31:38 +0200

    removed unnecessary constructor

--- src/font.cpp
+++ src/font.cpp
@@ -1,13 +1,6 @@
 #include "font.hpp"
 #include <string>
 
-Font::Font(const std::string& path, int size) {
-    m_font = TTF_OpenFont(path.c_str(), size);
-    if (m_font == nullptr) {
-        throw FontLoadingError{ "error loading font: '" + std::string{ TTF_GetError() } + "'" };
-    }
-}
-
 Font::Font(const std::filesystem::path& path, int size) {
     m_font = TTF_OpenFont(path.string().c_str(), size);
     if (m_font == nullptr) {
