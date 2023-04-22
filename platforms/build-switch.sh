28a86dc782c64b78b5649dd3457d926aa685719c
Totto16 <tobiausgais@gmail.com>
2023-04-22 02:59:16 +0200

    removed unnecessary commented code

--- platforms/build-switch.sh
+++ platforms/build-switch.sh
@@ -29,39 +29,3 @@ cp -r assets platforms/romfs/
 
 
 make -f ./platforms/switch.mk -j $(nproc)
-
-# cat <<EOF >"./android/crossbuilt-switch.ini"
-# [host_machine]
-# system = 'linux'
-# cpu_family = 'arm64'
-# cpu = 'aarch64'
-# endian = 'little'
-
-# [binaries]
-# c = '$CC'
-# cpp = '$CXX'
-# ar      = '$AR'
-# as      = '$AS'
-# ranlib  = '$RANLIB'
-# ld      = '$LD'
-# strip   = '$TOOL_PREFIX-strip'
-# objcopy = '$TOOL_PREFIX-objcop'
-# pkgconfig = '$DEVKITPRO/portlibs/switch/bin/$TOOL_PREFIX-pkg-config'
-# cmake = '$TOOL_PREFIX-cmake'
-
-# [built-in options]
-# c_std = 'c11'
-# cpp_std = 'c++20'
-# c_args = ['-fPIE','-fPIC']
-# cpp_args = ['-fPIE','-fPIC']
-# c_link_args = ['-fPIE']
-# cpp_link_args = ['-fPIE']
-
-# EOF
-
-# meson setup "$BUILD_DIR" \
-#     --cross-file "./android/crossbuilt-switch.ini" \
-#     -Dsdl2:use_hidapi=disabled \
-#     -Dsdl2:test=false
-
-# meson compile -C "$BUILD_DIR"
