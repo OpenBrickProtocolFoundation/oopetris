537ec0efd2d65860c5c30ab62f18b8ce66e8fbb5
Totto16 <tobiausgais@gmail.com>
2023-04-23 03:21:15 +0200

    added music assets + music

--- src/tetrion.cpp
+++ src/tetrion.cpp
@@ -11,6 +11,7 @@ Tetrion::Tetrion(
         const u8 tetrion_index,
         const Random::Seed random_seed,
         const int starting_level,
+        MusicManager* music_manager,
         tl::optional<RecordingWriter*> recording_writer
 )
     : m_tetrion_index{ tetrion_index },
@@ -19,7 +20,8 @@ Tetrion::Tetrion(
       m_level{ starting_level },
       m_next_gravity_simulation_step_index{ get_gravity_delay_frames() },
       m_recording_writer{ recording_writer },
-      m_lock_delay_step_index{ lock_delay } {
+      m_lock_delay_step_index{ lock_delay },
+      m_music_manager{ music_manager } {
 
     const auto font_path = utils::get_assets_folder() / "fonts" / "PressStart2P.ttf";
 #if defined(__ANDROID__)
@@ -163,6 +165,7 @@ void Tetrion::spawn_next_tetromino(const TetrominoType type, const SimulationSte
     refresh_previews();
     if (not is_active_tetromino_position_valid()) {
         m_game_state = GameState::GameOver;
+        m_music_manager->load_and_play_music(utils::get_assets_folder() / "music" / "05. Results.mp3");
         spdlog::info("game over");
         if (m_recording_writer.has_value()) {
             spdlog::info("writing snapshot");
@@ -291,6 +294,12 @@ void Tetrion::clear_fully_occupied_lines() {
                 if (level > m_level) {
                     m_level = level;
                     spdlog::info("new level: {}", m_level);
+                    //TODO don't hardcode and how do we get m_music_manager in here?
+                    if (m_lines_cleared == 1 or level == 30) {
+                        m_music_manager->load_and_play_music(
+                                utils::get_assets_folder() / "music" / "03. Game Theme (50 Left).flac"
+                        );
+                    }
                 }
                 m_mino_stack.clear_row_and_let_sink(row);
                 cleared = true;
