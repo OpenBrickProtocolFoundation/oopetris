198ce3adbc74335225356631b36b4e94e172f04e
Michael Gerhold <michael.gerhold@web.de>
2023-04-23 19:22:24 +0200

    removed magic constant

--- src/tetrion.cpp
+++ src/tetrion.cpp
@@ -15,7 +15,7 @@ Tetrion::Tetrion(
 )
     : m_tetrion_index{ tetrion_index },
       m_random{ random_seed },
-      m_grid{ Point{ static_cast<int>(5.5 * tile_size), tile_size / 2 }, tile_size },
+      m_grid{ Point{ static_cast<int>(tile_size / 2 + (Grid::hold_background_extends.x + 1) * tile_size), tile_size / 2 }, tile_size },
       m_level{ starting_level },
       m_next_gravity_simulation_step_index{ get_gravity_delay_frames() },
       m_recording_writer{ recording_writer },
