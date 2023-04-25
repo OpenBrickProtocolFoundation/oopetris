68ae5924a448aa715f9713fcf87769a1a23e4ef0
Totto16 <tobiausgais@gmail.com>
2023-04-25 21:23:45 +0200

    add notion of BUILD_INSTALLER, so that the installer can move it's files into a directory, where the application then can find them!

--- src/utils.cpp
+++ src/utils.cpp
@@ -34,13 +34,15 @@ namespace utils {
     }
 
     [[nodiscard]] std::filesystem::path get_root_folder() {
-#if defined(__ANDROID__)
+#if defined(__ANDROID__) or defined(BUILD_INSTALLER)
+        // this call also creates the dir (at least tries to) it returns
         char* pref_path = SDL_GetPrefPath(constants::author, constants::program_name);
         if (!pref_path) {
-            throw std::runtime_error{ "Failed in getting the Pref Path on android!" };
+            throw std::runtime_error{ "Failed in getting the Pref Path: " + std::string{ SDL_GetError() } };
         }
         return std::filesystem::path{ std::string{ pref_path } };
 #else
+        // this is only used in local build for debugging, when compiling in release mode the path is real path where the app can store many things without interfering with other things (eg. AppData\Roaming\... onw Windows or  .local/share/... on Linux )
         return std::filesystem::path{ "." };
 #endif
     }
@@ -48,6 +50,13 @@ namespace utils {
     [[nodiscard]] std::filesystem::path get_assets_folder() {
 #if defined(__ANDROID__)
         return std::filesystem::path{ "." };
+#elif defined(BUILD_INSTALLER)
+        char* pref_path = SDL_GetPrefPath(constants::author, constants::program_name);
+        if (!pref_path) {
+            throw std::runtime_error{ "Failed in getting the Pref Path: " + std::string{ SDL_GetError() } };
+        }
+        // if you build in BUILD_INSTALLER mode, you have to assure that the data is there eg. music  + fonts!
+        return std::filesystem::path{ std::string{ pref_path } } / "assets";
 #else
         return std::filesystem::path{ "assets" };
 #endif
