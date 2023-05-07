edd364a368b1694afa25fa79f04ff50346c5c971
Michael Gerhold <michael.gerhold@web.de>
2023-05-07 21:43:24 +0200

    deleted move operations

--- src/sdl_context.hpp
+++ src/sdl_context.hpp
@@ -3,7 +3,8 @@
 struct SdlContext final {
     SdlContext();
     SdlContext(const SdlContext&) = delete;
+    SdlContext(SdlContext&&) = delete;
     SdlContext& operator=(const SdlContext&) = delete;
-
+    SdlContext& operator=(SdlContext&&) = delete;
     ~SdlContext();
 };
