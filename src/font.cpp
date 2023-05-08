209366834f2f393531a701d10762b7fd4d9fc336
Totto16 <tobiausgais@gmail.com>
2023-05-08 22:20:17 +0200

    better font error handling (don't ask me why sdl fails at doing that exact thing 🤷‍♂️)

--- src/font.cpp
+++ src/font.cpp
@@ -1,9 +1,15 @@
 #include "font.hpp"
+#include <filesystem>
 #include <string>
 
 Font::Font(const std::filesystem::path& path, int size)
     : m_font{ TTF_OpenFont(path.string().c_str(), size), TTF_CloseFont } {
     if (m_font == nullptr) {
-        throw FontLoadingError{ "error loading font: '" + std::string{ TTF_GetError() } + "'" };
+        // this error is bogus if the file doesn't exist, don't ask me why
+        std::string error = std::string{ TTF_GetError() };
+        if (not std::filesystem::exists(path)) {
+            error = "path '" + path.string() + "' doesn't exist!";
+        }
+        throw FontLoadingError{ "error loading font: '" + error + "'" };
     }
 }
