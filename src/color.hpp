f60dc98cb4c0e6d52a7ae73a7b46fcc02c0a8922
Totto16 <tobiausgais@gmail.com>
2023-06-09 23:58:59 +0200

    replace some magical values by some hex representation, that suits it better

--- src/color.hpp
+++ src/color.hpp
@@ -12,25 +12,25 @@ struct Color {
 
     constexpr Color(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a) : r{ r }, g{ g }, b{ b }, a{ a } { }
 
-    constexpr Color(std::uint8_t r, std::uint8_t g, std::uint8_t b) : Color{ r, g, b, 255 } { }
+    constexpr Color(std::uint8_t r, std::uint8_t g, std::uint8_t b) : Color{ r, g, b, 0xFF } { }
 
-    static constexpr Color red(std::uint8_t alpha = 255) {
-        return Color{ 255, 0, 0, alpha };
+    static constexpr Color red(std::uint8_t alpha = 0xFF) {
+        return Color{ 0xFF, 0, 0, alpha };
     };
 
-    static constexpr Color green(std::uint8_t alpha = 255) {
-        return Color{ 0, 255, 0, alpha };
+    static constexpr Color green(std::uint8_t alpha = 0xFF) {
+        return Color{ 0, 0xFF, 0, alpha };
     };
 
-    static constexpr Color blue(std::uint8_t alpha = 255) {
-        return Color{ 0, 0, 255, alpha };
+    static constexpr Color blue(std::uint8_t alpha = 0xFF) {
+        return Color{ 0, 0, 0xFF, alpha };
     };
 
-    static constexpr Color black(std::uint8_t alpha = 255) {
+    static constexpr Color black(std::uint8_t alpha = 0xFF) {
         return Color{ 0, 0, 0, alpha };
     };
 
-    static constexpr Color white(std::uint8_t alpha = 255) {
-        return Color{ 255, 255, 255, alpha };
+    static constexpr Color white(std::uint8_t alpha = 0xFF) {
+        return Color{ 0xFF, 0xFF, 0xFF, alpha };
     };
 };
