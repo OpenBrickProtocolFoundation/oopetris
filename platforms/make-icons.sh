6573e0314f95d3aa7c42c55e8c61ae802c10abd0
Totto16 <tobiausgais@gmail.com>
2023-06-12 17:41:41 +0200

    add icons (rendered from svg)

---
+++ platforms/make-icons.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for RES in 24 64 96 160 192 256 512; do
+
+    inkscape "--export-width=$RES" "--export-height=$RES" --export-type=png "--export-filename=../assets/icon/icon_$RES.png" "../assets/OOPetris.svg"
+
+done
+
+MAX_RES="4096"
+inkscape "--export-width=$MAX_RES" "--export-height=$MAX_RES" --export-type=svg "--export-filename=../assets/icon/OOPetris.svg" "../assets/OOPetris.svg"
