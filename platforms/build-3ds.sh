bba96d11fb70a73b994efbeddfc5364aaf3e39d8
Totto16 <tobiausgais@gmail.com>
2024-01-17 00:53:21 +0100

    WIP: - add 3ds build - improve naming in switch build

--- platforms/build-3ds.sh
+++ platforms/build-3ds.sh
@@ -3,25 +3,26 @@
 set -e
 
 export DEVKITPRO=/opt/devkitpro
-export ARCH_DEVKIT_FOLDER="$DEVKITPRO/devkitA64"
+export ARCH_DEVKIT_FOLDER="$DEVKITPRO/devkitARM"
 export COMPILER_BIN="$ARCH_DEVKIT_FOLDER/bin"
+export ARCH_DEVKIT_LIB="$ARCH_DEVKIT_FOLDER/lib"
 export PATH="$DEVKITPRO/tools/bin:$COMPILER_BIN:$PATH"
 
 export PORTLIBS_PATH="$DEVKITPRO/portlibs"
-export LIBNX=$DEVKITPRO/libnx
+export LIBCTRU=$DEVKITPRO/libctru
 
-export PORTLIBS_LIB=$PORTLIBS_PATH/switch/lib
-export LIBNX_LIB=$LIBNX/lib
+export PORTLIBS_LIB=$PORTLIBS_PATH/3ds/lib
+export LIBCTRU_LIB=$LIBCTRU/lib
 
 export PKG_CONFIG_PATH="$PORTLIBS_LIB/pkgconfig/"
 
 export ROMFS=platforms/romfs
 
-export BUILD_DIR="build-switch"
+export BUILD_DIR="build-3ds"
 
-export TOOL_PREFIX=aarch64-none-elf
+export TOOL_PREFIX=arm-none-eabi
 
-export BIN_DIR="$PORTLIBS_PATH/switch/bin"
+export BIN_DIR="$PORTLIBS_PATH/3ds/bin"
 export PKG_CONFIG_EXEC=$BIN_DIR/$TOOL_PREFIX-pkg-config
 export CMAKE="$BIN_DIR/$TOOL_PREFIX-cmake"
 
@@ -39,19 +40,19 @@ export STRIP="$COMPILER_BIN/$TOOL_PREFIX-strip"
 # cpompat flags for some POSIX functions
 export COMPAT_FLAGS="'-D_XOPEN_SOURCE'"
 
-export ARCH=aarch64
-export ARM_VERSION=armv8-a
-export COMMON_FLAGS="'-D__SWITCH__','-ffunction-sections','-fdata-sections', $COMPAT_FLAGS"
+export ARCH=arm
+export ARM_VERSION=armv6k
+export COMMON_FLAGS="'-D__3DS__','-mword-relocations', '-ffunction-sections','-mword-relocations', $COMPAT_FLAGS"
 
-export COMPILE_FLAGS="'-march=armv8-a+crc+crypto','-mtune=cortex-a57','-mtp=soft','-ftls-model=local-exec','-fPIC', '-I$LIBNX/include'"
+export COMPILE_FLAGS="'-march=armv6k','-mtune=mpcore','-mfloat-abi=hard', '-mtp=soft','-fPIC', '-I$LIBCTRU/include'"
 
-export LIBS_FLAGS="'-L$PORTLIBS_LIB','-L$LIBNX_LIB','-fPIE','-specs=$DEVKITPRO/libnx/switch.specs'"
+export LIBS_FLAGS="'-L$PORTLIBS_LIB','-L$LIBCTRU_LIB','-fPIE','-specs=$ARCH_DEVKIT_FOLDER/$TOOL_PREFIX/lib/3dsx.specs'"
 
-export CROSS_FILE="./platforms/crossbuild-switch.ini"
+export CROSS_FILE="./platforms/crossbuild-3ds.ini"
 
 cat <<EOF >"$CROSS_FILE"
 [host_machine]
-system = 'switch'
+system = '3ds'
 cpu_family = '$ARCH'
 cpu = '$ARM_VERSION'
 endian = 'little'
@@ -87,19 +88,16 @@ cpp_link_args = [$COMMON_FLAGS, $LIBS_FLAGS]
 [properties]
 pkg_config_libdir = '$PKG_CONFIG_PATH'
 needs_exe_wrapper = true
-library_dirs= ['$LIBNX_LIB', '$PORTLIBS_LIB']
+library_dirs= ['$LIBCTRU_LIB', '$PORTLIBS_LIB']
 romfs_dir='$ROMFS'
-libnx='$LIBNX'
+libctru='$LIBCTRU'
 
 APP_ICON='$ROMFS/assets/icon/icon_512.png'
+APP_SMALL_ICON = '$ROMFS/assets/icon/icon_128.png'
 APP_TITLE	= 'oopetris'
 APP_AUTHOR 	= 'coder2k'
 APP_VERSION = '1.0'
-
-# optional
-APP_TITLEID = ''
-
-USE_NACP    = true
+APP_DESC = 'OOP Tetris'
 
 EOF
 
