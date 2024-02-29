4686fa4df3b7bb952a9a66e9edd479270e61368e
Totto16 <tobiausgais@gmail.com>
2024-02-29 04:02:55 +0100

    fic mac build

--- src/thirdparty/hash-library/sha256.cpp
+++ src/thirdparty/hash-library/sha256.cpp
@@ -12,12 +12,14 @@
 #if defined(_MSC_VER)
 
 #elif defined(__SWITCH__)
-#if ! (defined(__BYTE_ORDER__) or defined(__ORDER_BIG_ENDIAN__))
+#if !(defined(__BYTE_ORDER__) or defined(__ORDER_BIG_ENDIAN__))
 #error "__BYTE_ORDER__ or __ORDER_BIG_ENDIAN__ not defined"!
 #else
 #define __BYTE_ORDER __BYTE_ORDER__
 #define __BIG_ENDIAN __ORDER_BIG_ENDIAN__
 #endif
+#elif defined(__APPLE__)
+#include <machine/endian.h>
 #else
 #include <endian.h>
 #endif
