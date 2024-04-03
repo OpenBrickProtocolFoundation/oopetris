c87ccdbb3a619fb0e45042090bc8352899de2a53
Totto16 <tobiausgais@gmail.com>
2024-04-04 00:27:37 +0200

    fix clang-tidy errors

--- src/helper/message_box.cpp
+++ src/helper/message_box.cpp
@@ -11,11 +11,11 @@ void helper::MessageBox::show_simple(
         SDL_Window* window
 ) {
 
-    Uint32 flags = type == Type::Error     ? SDL_MESSAGEBOX_ERROR
-                   : type == Type::Warning ? SDL_MESSAGEBOX_WARNING
-                                           : SDL_MESSAGEBOX_INFORMATION;
+    const Uint32 flags = type == Type::Error     ? SDL_MESSAGEBOX_ERROR
+                         : type == Type::Warning ? SDL_MESSAGEBOX_WARNING
+                                                 : SDL_MESSAGEBOX_INFORMATION;
 
-    int result = SDL_ShowSimpleMessageBox(flags, title.c_str(), content.c_str(), window);
+    const int result = SDL_ShowSimpleMessageBox(flags, title.c_str(), content.c_str(), window);
 
     if (result < 0) {
         throw std::runtime_error{ fmt::format("Failed to show Simple MessageBox: {}", SDL_GetError()) };
