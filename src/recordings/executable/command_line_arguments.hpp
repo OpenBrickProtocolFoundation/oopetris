d53005df939193947097f5eeab6fdad30feddd29
Totto16 <tobiausgais@gmail.com>
2024-04-04 16:31:54 +0200

    add src/recordings, due to gitignore issue, they where ignored before

---
+++ src/recordings/executable/command_line_arguments.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "helper/optional.hpp"
+
+#include <argparse/argparse.hpp>
+#include <filesystem>
+#include <fmt/format.h>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+
+enum class Command { Info, Dump };
+
+struct CommandLineArguments final {
+private:
+public:
+    std::filesystem::path recording_path{};
+    Command command{ Command::Info };
+
+    CommandLineArguments(int argc, char** argv) {
+        argparse::ArgumentParser parser{ argc >= 1 ? argv[0] //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+                                                   : "oopetris_recording_utility",
+                                         "0.0.1", argparse::default_arguments::all };
+
+        parser.add_argument("command")
+                .help("specify the command")
+                .default_value(std::string{ "info" })
+                .choices("info", "i", "dump", "d")
+                .required();
+
+        parser.add_argument("-r", "--recording").help("the path of a recorded game file").required();
+        try {
+
+            parser.parse_args(argc, argv);
+
+            recording_path = parser.get("--recording");
+
+            const auto command_string = parser.get<std::string>("command");
+
+
+            if (command_string == "info" or command_string == "i") {
+                command = Command::Info;
+            } else if (command_string == "dump" || command_string == "d") {
+                command = Command::Dump;
+            } else {
+                throw std::runtime_error(fmt::format("Unknown command: '{}'", command_string));
+            }
+        } catch (const std::exception& err) {
+            std::cerr << err.what();
+            std::exit(1);
+        }
+    }
+};
---
+++ src/recordings/executable/main.cpp
@@ -0,0 +1,21 @@
+
+#include "command_line_arguments.hpp"
+#include "recording_reader.hpp"
+
+#include <filesystem>
+#include <iostream>
+
+int main(int argc, char** argv) noexcept {
+
+    const auto arguments = CommandLineArguments(argc, argv);
+
+    if (not std::filesystem::exists(arguments.recording_path)) {
+        std::cerr << arguments.recording_path << " does not exist!\n";
+        return 1;
+    }
+
+
+    const auto parsed = recorder::RecordingReader::from_path(arguments.recording_path);
+
+    return 0;
+}
