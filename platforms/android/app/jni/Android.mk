ac798c506c7c2ef4f3fa0a6b539044bc3e465a44
Totto16 <tobiausgais@gmail.com>
2024-04-04 17:21:29 +0200

    fix android compilation and add now new needed libs

--- platforms/android/app/jni/Android.mk
+++ platforms/android/app/jni/Android.mk
@@ -83,6 +83,27 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := liboopetris_core
+LIB_PATH := $(BUILD_PATH)/src
+LOCAL_SRC_FILES := $(LIB_PATH)/liboopetris_core.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liboopetris_recordings
+LIB_PATH := $(BUILD_PATH)/src
+LOCAL_SRC_FILES := $(LIB_PATH)/liboopetris_recordings.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liboopetris_graphics
+LIB_PATH := $(BUILD_PATH)/src
+LOCAL_SRC_FILES := $(LIB_PATH)/liboopetris_graphics.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := liboopetris
 LOCAL_SRC_FILES := $(BUILD_PATH)/liboopetris.so
 include $(PREBUILT_SHARED_LIBRARY)
@@ -91,7 +112,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := main
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf freetype libpng SDL2_mixer libvorbis libvorbisfile libogg libflac SDL2_image libfmt liboopetris
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf freetype libpng SDL2_mixer libvorbis libvorbisfile libogg libflac SDL2_image libfmt liboopetris_core liboopetris_recordings liboopetris_graphics liboopetris
 LOCAL_LDLIBS := -ldl -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid
 LOCAL_LDFLAGS := -Wl,--no-undefined
 include $(BUILD_SHARED_LIBRARY)
