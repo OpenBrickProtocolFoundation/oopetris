cc743d29627b1c07564bff177f2f03f3ab660141
Totto16 <tobiausgais@gmail.com>
2024-05-11 16:44:11 +0200

    android: update to clang 18, by using ndk 27 beta1

--- platforms/build-android.sh
+++ platforms/build-android.sh
@@ -4,8 +4,8 @@ set -e
 
 mkdir -p toolchains
 
-export NDK_VER_DOWNLOAD="r26d"
-export NDK_VER_DESC="r26d"
+export NDK_VER_DOWNLOAD="r27-beta1"
+export NDK_VER_DESC="r27-beta1"
 
 export BASE_PATH="$PWD/toolchains/android-ndk-$NDK_VER_DESC"
 export ANDROID_NDK_HOME="$BASE_PATH"
