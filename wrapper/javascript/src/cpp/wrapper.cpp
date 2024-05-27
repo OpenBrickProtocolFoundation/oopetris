163b8cdc6d4e88f541326d57fd52e64668fc837a
Totto16 <tobiausgais@gmail.com>
2024-05-27 02:43:36 +0200

    Wrapper, JS: fix clang builds

--- wrapper/javascript/src/cpp/wrapper.cpp
+++ wrapper/javascript/src/cpp/wrapper.cpp
@@ -4,7 +4,17 @@
 #endif
 #endif
 
+#if defined(__GNUC__) & !defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wtemplate-id-cdtor"
+#endif
+
 #include <nan.h>
+
+#if defined(__GNUC__) & !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
+
 
 #include <core/core.hpp>
 #include <recordings/recordings.hpp>
