09478feb62871e6001f69223465f27fcbe51745a
Totto16 <tobiausgais@gmail.com>
2024-06-05 00:54:01 +0200

    android: support 16KB page sizes (is used in newer android version, previously it was 4KB)

--- platforms/android/app/jni/Application.mk
+++ platforms/android/app/jni/Application.mk
@@ -6,4 +6,8 @@ APP_STL := c++_shared
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 
 # used SDK number
-APP_PLATFORM=android-34
+APP_PLATFORM := android-34
+
+# support 16KB page sizes:
+# see: https://developer.android.com/guide/practices/page-sizes 
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
