0a1bb668e39e77796ce60d033d1ef3e9c4f4e837
Totto16 (Windows VM) <tobiausgais@gmail.com>
2024-10-15 00:59:25 +0200

    windows: fix static builds, by some define magic

--- src/libs/core/helper/windows.hpp
+++ src/libs/core/helper/windows.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
 #if defined(_MSC_VER)
+#if defined(OOPETRIS_LIBRARY_CORE_COMPILATION)
 #if defined(OOPETRIS_LIBRARY_CORE_EXPORT)
 #define OOPETRIS_CORE_EXPORTED __declspec(dllexport)
 #else
 #define OOPETRIS_CORE_EXPORTED __declspec(dllimport)
+#endif
+#else
+#define OOPETRIS_CORE_EXPORTED
 #endif
 #else
 #define OOPETRIS_CORE_EXPORTED
