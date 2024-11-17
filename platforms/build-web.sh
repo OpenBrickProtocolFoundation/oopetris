13c8e72b8a424a79ca078cffc7d7bba8525ef07e
Totto16 <tobiausgais@gmail.com>
2024-11-18 00:22:19 +0100

    fix: emscripten use correct port names (not varianet names)

--- platforms/build-web.sh
+++ platforms/build-web.sh
@@ -58,9 +58,9 @@ export ENDIANESS="little"
 
 export ROMFS="platforms/romfs"
 
-export PACKAGE_FLAGS="'--use-port=sdl2-mt', '--use-port=harfbuzz-mt', '--use-port=freetype', '--use-port=zlib', '--use-port=sdl2_ttf', '--use-port=mpg123', '--use-port=sdl2_mixer_mp3', '-sSDL2_MIXER_FORMATS=[\"mp3\"]','--use-port=libpng-mt', '--use-port=sdl2_image','-sSDL2_IMAGE_FORMATS=[\"png\",\"svg\"]', '--use-port=icu-mt'"
+export PACKAGE_FLAGS="'--use-port=sdl2', '--use-port=harfbuzz', '--use-port=freetype', '--use-port=zlib', '--use-port=sdl2_ttf', '--use-port=mpg123', '--use-port=sdl2_mixer', '-sSDL2_MIXER_FORMATS=[\"mp3\"]','--use-port=libpng', '--use-port=sdl2_image','-sSDL2_IMAGE_FORMATS=[\"png\",\"svg\"]', '--use-port=icu'"
 
-export COMMON_FLAGS="'-fexceptions', '-pthread', '-sUSE_PTHREADS=1', '-sPTHREADS=1','-sEXCEPTION_CATCHING_ALLOWED=[..]', $PACKAGE_FLAGS"
+export COMMON_FLAGS="'-fexceptions', '-pthread', '-sUSE_PTHREADS=1', '-sEXCEPTION_CATCHING_ALLOWED=[..]', $PACKAGE_FLAGS"
 
 # TODO see if ALLOW_MEMORY_GROWTH is needed, but if we load ttf's and music it likely is and we don't have to debug OOm crahses, that aren't handled by some thrid party library, which is painful
 export LINK_FLAGS="$COMMON_FLAGS, '-sEXPORT_ALL=1', '-sUSE_WEBGPU=1', '-sWASM=1', '-sALLOW_MEMORY_GROWTH=1', '-sASSERTIONS=1','-sERROR_ON_UNDEFINED_SYMBOLS=1', '-sFETCH=1'"
