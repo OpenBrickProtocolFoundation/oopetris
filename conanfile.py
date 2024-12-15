cff5a5b7d8ce61f4c3b951a836f5efb6c0d1b95d
Totto16 <tobiausgais@gmail.com>
2024-12-15 03:17:55 +0100

    conanfile: correctly require tl-expected, when the compiler doesn't support std::expected

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,8 @@
 from conan import ConanFile
-from conan.tools.build import check_min_cppstd
+from conan.tools.build import check_min_cppstd, valid_min_cppstd
 from conan.tools.meson import Meson, MesonToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.gnu import PkgConfig, PkgConfigDeps
+from conan.tools.gnu import PkgConfigDeps
 
 
 class liboopetris(ConanFile):
@@ -48,6 +48,8 @@ class liboopetris(ConanFile):
         self.requires("nlohmann_json/[>=3.11.0]")
         self.requires("magic_enum/[>=0.9.0]")
         self.requires("utfcpp/[>=4.0.0]")
+        if not valid_min_cppstd(self, "23"):
+            self.requires("tl-expected/[>=1.0.0]")
 
     def build(self) -> None:
         meson = Meson(self)
