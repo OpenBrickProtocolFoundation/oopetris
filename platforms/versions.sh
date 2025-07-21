a78cd6c842e52f4a7df460ae1468e802aea5eee1
Totto16 <tobiausgais@gmail.com>
2025-07-21 17:16:49 +0200

    feat: add versions.sh file, that store the used versions for build scripts

---
+++ platforms/versions.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+## 3ds section
+
+export SDL_3DS_VERSION="2.30.6"
+
+export SDL_TTF_3DS_VERSION="2.22.0"
+
+export SDL_MIXER_3DS_VERSION="2.8.0"
+
+export SDL_IMAGE_3DS_VERSION="2.8.2"
