09f66f270b3a43d6fefcf48a57fda5228e425556
Totto16 <tobiausgais@gmail.com>
2026-09-07 00:44:33 +0200

    feat: add runtime target to build .img file for easier flashing of the resulting efi file

--- platforms/uefi/create-fat-folder.sh
+++ platforms/uefi/create-fat-folder.sh
@@ -34,4 +34,40 @@ if ! [ -e "$DEST_FILE" ]; then
     cp -p -f "$EFI_FILE" "$DEST_FILE"
 fi
 
+# create a grub config to boot this
+
+GRUB_BOOT_FOLDER_LOC="$FAT_FOLDER_LOC/boot/grub/"
+
+mkdir -p "$GRUB_BOOT_FOLDER_LOC"
+
+OOPETRIS_PART_FILE="$GRUB_BOOT_FOLDER_LOC/oopetris_marker.txt"
+
+echo "OOPETRIS MARKER" >"$OOPETRIS_PART_FILE"
+
+GRUB_CFG_FILE="$GRUB_BOOT_FOLDER_LOC/grub.cfg"
+
+cat <<EOF >"$GRUB_CFG_FILE"
+menuentry "Boot OOPetris EFI" --class efi {
+    ## not sure which of these are really needed, but load them, so we don#t rely on defaults
+    insmod boot
+    insmod chain
+    insmod fat
+    insmod msdospart
+    insmod search
+    insmod search_fs_file
+    # search for the marker file and then set the root to that partition
+    search --no-floppy --file --set=root /boot/grub/oopetris_marker.txt
+    # set this file as boot entry
+    chainloader /EFI/BOOT/BOOTX64.EFI
+    # boot the file
+    boot
+}
+EOF
+
+#NOTE: this makes booting easier, just run
+#'configfile /boot/grub/grub.cfg' in the grub shell
+# or maybe grub detects this config file and show it in the graphical interface (?)
+# TODO: when does grub do this?
+# NOTE: select the disk in the grub shell by using ls and then "(hd0,msdos)/.."
+
 touch "$PSEUDO_OUTPUT"
---
+++ platforms/uefi/create-img-file.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+## Treat undefined variables as an error
+set -u
+# fails if any part of a pipeline (|) fails
+set -o pipefail
+
+SCRIPT_DIR="$(realpath "$(dirname -- "${BASH_SOURCE[0]}")")"
+
+# shellcheck source=./platforms/helper.sh
+source "$SCRIPT_DIR/../helper.sh"
+
+if [ "$#" -eq 2 ]; then
+    FAT_FOLDER_LOC="$(realpath "$1")"
+    IMG_FILE="$(realpath "$2")"
+else
+    echo "Too many arguments given, expected 2" >&2
+    exit 1
+fi
+
+set -x
+
+SIZE_MB=100
+
+validate_parent_dir "$IMG_FILE"
+
+rm -f "$IMG_FILE"
+
+# Create empty image
+dd "if=/dev/zero" "of=$IMG_FILE" "bs=1M" "count=$SIZE_MB" "status=progress"
+
+# 2048 sectors = 1 MiB offset
+START_SECTOR_AMOUNT="2048"
+SECTOR_SIZE="512"
+PART_START="$((START_SECTOR_AMOUNT * 1))"
+PART_END="$(((SIZE_MB * START_SECTOR_AMOUNT) - 1))"
+
+# Create MBR partition table and one ESP (EF) partition, with the boot flag enabled
+# Partition starts at 1 MiB for proper alignment.
+fdisk "$IMG_FILE" <<EOF
+o
+n
+p
+1
+$PART_START
+$PART_END
+t
+uefi
+a
+w
+EOF
+
+# make fat32 partition
+mkfs.fat -a -S "$SECTOR_SIZE" -F 32 --offset="$PART_START" "$IMG_FILE"
+
+PART_START_BYTES="$((START_SECTOR_AMOUNT * SECTOR_SIZE))"
+
+mcopy -i "${IMG_FILE}@@$PART_START_BYTES" -s "$FAT_FOLDER_LOC" "::/"
+
+mdir -i "${IMG_FILE}@@$PART_START_BYTES" "::"
+
+fdisk -l "$IMG_FILE"
